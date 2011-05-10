f2accdc0c7fa47ce2b7dd7a662cfdd54333a57fd
Matt Sain <matt@insaindesign.com>
2011-05-10 10:10:17 +1000

    commenting code

--- killie.js
+++ killie.js
@@ -1,11 +1,14 @@
 /*
 	Death to incompatibility
-	This will detect ie 6 and offer better alternatives
+	This will offer better alternatives to an unsupported browser
 	Freely distributable under MIT-style license to use, modify or publish as required
 */
-(function () {
+(function (WIN, DOC) {
+	/*************************/
+	/* Configure the message */
+	/*************************/
 	var CSS_FILE = 'http://accept.sydney.corp.yahoo.com/~sainm/js/ie/style.css',
-		MESSAGE = "Your browser is not supported!",
+		MESSAGE_TITLE = "Your browser is not supported!",
 		MESSAGE_OPTIONS = "To start enjoying a better and more secure web we recommend installing:",
 		BROWSERS = [
 			{ 
@@ -26,12 +29,17 @@
 			}
 		],
 		MESSAGE_FRAME = "If you're unable to install a new browser, try the <a href=\"http://www.google.com/chromeframe/\" class=\"icon chrome-frame\">Chrome Frame</a> plugin.",
-		//document head
-		msg = document.createElement('div'),
+		
+		
+	/**********************************/
+	/* No need to configure this part */	
+	/**********************************/
+		
+		msg = DOC.createElement('div'),
 		msgAnim = null,
 		Animation, il, i;
 		
-	//finish off the message
+	//finish off the message options by adding the browsers in the order provided above
 	for (i = 0, il = BROWSERS.length; i < il; i += 1) {
 		if (i) {
 			MESSAGE_OPTIONS += (i + 1 < il) ? ',' : ' or';
@@ -39,6 +47,7 @@
 		MESSAGE_OPTIONS += ' <a class="icon ' + BROWSERS[i].name.toLowerCase() + '" href="' + BROWSERS[i].href + '">' + BROWSERS[i].name + '</a>';
 	}
 	
+	//quick and dirty animation class
 	Animation = function (el, duration) { 
 		var properties = {}, 
 			interval = 25, 
@@ -82,16 +91,17 @@
 			}
 		};
 	};
-		
+	
+	//css file loader with callback on complete
 	function loadCSS(url, callback){
-		var file = document.createElement('link'),
-			html = document.getElementsByTagName('html')[0], 
-			img = document.createElement('img');
+		var file = DOC.createElement('link'),
+			html = DOC.getElementsByTagName('html')[0], 
+			img = DOC.createElement('img');
 		//setup the file params
 		file.type = 'text/css';
 		file.rel = 'stylesheet';
 		file.href = url;
-		document.getElementsByTagName('head')[0].appendChild(file);
+		DOC.getElementsByTagName('head')[0].appendChild(file);
 		if (callback) {
 			img.src = url;
 			img.onerror = function(){
@@ -112,32 +122,35 @@
 	}
 	
 	function init() {
-		var inner = document.createElement('div'),
-			cls = document.createElement('a');
+		var inner = DOC.createElement('div'),
+			cls = DOC.createElement('a');
 		//close button
 		cls.className = 'close';
 		cls.innerHTML = '<strong>x</strong><span> close</span>';
 		cls.onclick = hide;
 		//the inner content
 		inner.className = "inner";
-		inner.innerHTML = '<h3>' + MESSAGE + '</h3><p>' + MESSAGE_OPTIONS + '</p><p>' + MESSAGE_FRAME + '</p>';
+		inner.innerHTML = '<h3>' + MESSAGE_TITLE + '</h3><p>' + MESSAGE_OPTIONS + '</p><p>' + MESSAGE_FRAME + '</p>';
 		inner.appendChild(cls);
 		//message
 		msg.id = 'browser-user-message';
 		msg.appendChild(inner);
+		//create the global animation class that we will use
 		msgAnim = new Animation(msg, 700);
-		//add css and fire the show event when loaded	
+		//add css and fire the show event when the css is loaded	
 		loadCSS(CSS_FILE, function () {
-			//wait till the css is ready
-			document.body.appendChild(msg);
+			DOC.body.appendChild(msg);
+			//hide it by default
 			msg.style.top = '-' + msg.offsetHeight + 'px';
+			//scroll it down
 			show();
 		});
 	}
+	
 	//wait till page is loaded
-	if (window.addEventListener) {
-		window.addEventListener('load', init, false);
+	if (WIN.addEventListener) {
+		WIN.addEventListener('load', init, false);
 	} else {
-		window.attachEvent('onload', init);
+		WIN.attachEvent('onload', init);
 	}
-}());
+}(window, document));
